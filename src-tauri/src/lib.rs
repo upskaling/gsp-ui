63a3622358df7c62ca328067c8a54d28c2f12c79
upskaling <upskaling@users.noreply.github.com>
2026-07-19 12:29:02 +0200

    Ajouter une icône dans le tray.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,69 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use tauri::menu::{Menu, MenuItem};
+use tauri::tray::TrayIconBuilder;
+use tauri::Manager;
+use tauri::image::Image;
+
+fn load_icon_for_tray() -> Result<Image<'static>, tauri::Error> {
+    let icon_bytes = include_bytes!("../icons/icon.png");
+    let img = image::load_from_memory(icon_bytes)
+        .map_err(|e| tauri::Error::AssetNotFound(format!("Failed to load icon: {}", e)))?;
+
+    let img_rgba = img.to_rgba8();
+    let (width, height) = img_rgba.dimensions();
+
+    let rgba_data = img_rgba.into_raw();
+    let static_data: &'static [u8] = Box::leak(rgba_data.into_boxed_slice());
+
+    Ok(Image::new(static_data, width, height))
+}
+
+fn setup_tray(app: &tauri::App) -> Result<(), tauri::Error> {
+    let app_handle = app.handle().clone();
+
+    let show = MenuItem::with_id(app, "show", "Afficher", true, None::<&str>)?;
+    let quit = MenuItem::with_id(app, "quit", "Quitter", true, None::<&str>)?;
+    let sep = tauri::menu::PredefinedMenuItem::separator(app)?;
+
+    let menu = Menu::with_items(app, &[&show, &sep, &quit])?;
+
+    let icon = load_icon_for_tray()?;
+
+    TrayIconBuilder::with_id("main-tray")
+        .menu(&menu)
+        .icon(icon)
+        .on_menu_event(move |app, event| {
+            match event.id.0.as_str() {
+                "quit" => {
+                    app.exit(0);
+                }
+                "show" => {
+                    if let Some(window) = app.get_webview_window("main") {
+                        let _ = window.show();
+                        let _ = window.set_focus();
+                    }
+                }
+                _ => {}
+            }
+        })
+        .on_tray_icon_event(move |_app, event| {
+            use tauri::tray::TrayIconEvent;
+
+            if matches!(event, TrayIconEvent::Click { .. }) {
+                if let Some(window) = app_handle.get_webview_window("main") {
+                    if window.is_visible().unwrap_or(false) {
+                        let _ = window.hide();
+                    } else {
+                        let _ = window.show();
+                        let _ = window.set_focus();
+                    }
+                }
+            }
+        })
+        .build(app)?;
+
+    Ok(())
+}
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
@@ -8,6 +73,18 @@ fn greet(name: &str) -> String {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
+        .setup(|app| {
+            if let Err(e) = setup_tray(app) {
+                eprintln!("Erreur lors de la création de la tray-icon: {}", e);
+            }
+            Ok(())
+        })
+        .on_window_event(|window, event| {
+            if let tauri::WindowEvent::CloseRequested { api, .. } = event {
+                api.prevent_close();
+                window.hide().unwrap();
+            }
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
