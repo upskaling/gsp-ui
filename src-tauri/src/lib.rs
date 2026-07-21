dbf3c24ffab7bb5679ecdb99ec16e825bfc4bba1
upskaling <upskaling@users.noreply.github.com>
2026-07-21 15:49:30 +0200

    Ajoute un plugin de Logue.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -763,6 +763,7 @@ fn speak_clipboard(
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_log::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_global_shortcut::Builder::new().build())
         .manage(Mutex::new(PlaybackState { _dummy: 0 }))
